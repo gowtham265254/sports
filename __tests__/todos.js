142a47331d05d731e6c1efd97a3252a2ef8f9210
Gowtham Prashath <106882708+gowtham265254@users.noreply.github.com>
2023-04-04 15:54:30 +0530

    Add files via upload

---
+++ __tests__/todos.js
@@ -0,0 +1,51 @@
+const request = require("supertest");
+var cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+const login = async (agent, username, password) => {
+  let res = await agent.get("/login");
+  let csrfToken = extractCsrfToken(res);
+  res = await agent.post("/session").send({
+    email: username,
+    password: password,
+    _csrf: csrfToken,
+  });
+};
+
+describe("Todo Application", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(4000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  test("Sign up", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/users").send({
+      firstName: "seeni",
+      lastName: "vasan",
+      email: "user123@gmail.com",
+      password: "12345678",
+      "_csrf": csrfToken, //prettier-ignore
+    });
+    expect(res.statusCode).toBe(302);
+  });
+});
